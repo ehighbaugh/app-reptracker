049de37245fe501205dc681fa80b809602ed374e
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-12-08 01:37:54 -0500

    Initializing application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { MyAnimalsComponent } from './my-animals/my-animals.component';
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    MyAnimalsComponent,
+    AnimalDetailComponent
   ],
   imports: [
     BrowserModule,
